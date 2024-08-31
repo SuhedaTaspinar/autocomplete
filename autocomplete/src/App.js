6659695120902168be0b2434dc5a9457d7ca2f6b
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-08-31 13:42:54 +0300

    Implemented search functionality with dynamic filtering based on user input using useState and useEffect.

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+const data = [
+    {
+        id: 1,
+        title: "test 1"
+    },
+    {
+        id: 2,
+        title: "Test 2"
+    },
+    {
+        id: 3,
+        title: "deneme 1"
+    },
+    {
+        id: 4,
+        title: "Deneme 2"
+    }
+]
+
 
 function App() {
+
+    const [search, setSearch] = useState("")
+    const [result, setResult] = useState([])
+
+    useEffect(() => {
+        if (search) {
+            setResult(data.filter(item => item.title.toLowerCase().includes(search.toLowerCase())))
+        }
+        else {
+            setResult([])
+        }
+    },[search])
+
     return (
-        <div>
-            App
+        <div className="search">
+            <input type="text" placeholder="Bir şeyler ara..." onChange={(e) => setSearch(e.target.value)}/>
+            {result && (
+                <div className="search-result">
+                    {result.map(item => (
+                    <div key={item.id} className="search-result-item">{item.title}</div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
